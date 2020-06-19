ac7b5a66026089a46bac50ee8c7ae74bf7353b9b
HafizMursaleenNagori <64045420+HafizMursaleenNagori@users.noreply.github.com>
2020-06-19 16:16:37 +0500

    Add files via upload

---
+++ build/precache-manifest.5d02aa6d7b77e6918e366c7f61ef0434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca497cd2c1f72b12232a99db624ce14a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce141040ea462b5e8463",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71d303612ca76a434c29",
+    "url": "/static/js/2.b07fbab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b07fbab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce141040ea462b5e8463",
+    "url": "/static/js/main.19b928f3.chunk.js"
+  },
+  {
+    "revision": "c927d757bc8632987e60",
+    "url": "/static/js/runtime-main.742f85e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
